f61c37eba9789a9a62cb1b0de9150907bd74b0de
shira19-meet <shira19@meet.mit.edu>
2017-07-31 17:29:21 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,47 @@
+import turtle
+turtle.shape("turtle")
+triangle = turtle.clone()
+triangle.shape("triangle")
+triangle.goto(100,100)
+
+##
+##
+##square.goto(0,100)
+##square.goto(100,100)
+##square.goto(100,0)
+##square.goto(0,0)
+##square.penup()
+##
+##turtle.shape("turtle")
+##triangle = turtle.clone()
+##square.shape("triangle")
+##square.penup()
+##triangle.goto(0,0)
+##triangle.goto(60,60)
+##triangle.goto(120,0)
+##            
+##triangle.goto(0,0)
+##triangle.penup()
+##
+
+triangle.shape("triangle")
+triangle.goto(100,100)
+triangle.goto(300,300)
+triangle.stamp()
+triangle.goto(100,100)
+
+UP=0
+LEFT=1
+DOWN=2
+RIGHT=3
+direction="UP"
+UP_ARROW="Up"
+LEFT_ARROW="Left"
+DOWN_ARROW="Down"
+RIGHT_ARROW="Right"
+SPACEBAR="space"
+UP=0
+direction=UP
+
+
+
